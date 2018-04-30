cd0b2c603c28a641ce209878e136bd9c950f0611
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-04-29 20:51:56 -0500

    JSON serializer for config

---
+++ Helpful Framework/Extensions/FrameworkConfigExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Helpful.Framework.Json
+{
+    /// <summary>Provides extensions method for <see cref="FrameworkBotConfig"/></summary>
+    public static class FrameworkConfigExtensions
+    {
+        /// <summary>Serializes this config into JSON with an optional factory for setting serializer settings.</summary>
+        public static string Serialize(this FrameworkBotConfig config, Action<JsonSerializerSettings> factory = null)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                ContractResolver = ConfigContractResolver.Instance
+            };
+
+            factory?.Invoke(settings);
+            return JsonConvert.SerializeObject(config, settings);
+        }
+    }
+}
---
+++ Helpful Framework/Json/ConfigContractResolver.cs
@@ -0,0 +1,33 @@
+﻿using Discord.Net.Rest;
+using Discord.Net.Udp;
+using Discord.Net.WebSockets;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Reflection;
+
+namespace Helpful.Framework.Json
+{
+    internal class ConfigContractResolver : DefaultContractResolver
+    {
+        public static readonly ConfigContractResolver Instance = new ConfigContractResolver();
+
+        public ConfigContractResolver()
+        {
+            IgnoreSerializableInterface = true;
+        }
+
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            if (typeof(MulticastDelegate).IsAssignableFrom(member.DeclaringType)) return null;
+            JsonProperty property = base.CreateProperty(member, memberSerialization);
+            if ((property.DeclaringType == typeof(WebSocketProvider) && property.PropertyName == "WebSocketProvider")
+                || (property.DeclaringType == typeof(UdpSocketProvider) && property.PropertyName == "UdpSocketProvider")
+                || (property.DeclaringType == typeof(RestClientProvider) && property.PropertyName == "RestClientProvider"))
+            {
+                property.Ignored = true;
+            }
+            return property;
+        }
+    }
+}
