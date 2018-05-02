86ed4b0af0d50027e316f0122370f6f143524129
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-05-02 08:56:15 -0500

    Fixes StartInternalAsync not returning

--- Helpful Framework/FrameworkBot.cs
+++ Helpful Framework/FrameworkBot.cs
@@ -136,7 +136,9 @@ namespace Helpful.Framework
             else
                 await CastInternal().StartAsync();
 
-            await Task.Run(async () => await StartConsoleAsync());
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            Task.Run(() => StartConsoleAsync());
+#pragma warning restore CS4014
         }
 
         /// <summary>
