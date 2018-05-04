372893a05a91bed2e7074ab0de3f6db1f279d873
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-05-03 19:05:05 -0500

    Starting new service map, dunno where it'll go

---
+++ Helpful Framework/ServiceMap.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Helpful.Framework
+{
+    /// <summary>Not ready for use.</summary>
+    internal class ServiceProvider : IServiceProvider
+    {
+        private ConcurrentDictionary<Type, object> _services { get; }
+        private List<ServiceDescriptor> _descriptors { get; }
+
+        public T GetService<T>() => (T)GetService(typeof(T));
+        public object GetService(Type serviceType)
+        {
+            var desc = _descriptors.First(s => s.ServiceType == serviceType);
+            switch (desc.Lifetime)
+            {
+                case ServiceLifetime.Singleton:
+                    return _services.GetOrAdd(desc.ServiceType, desc.ImplementationFactory(this));
+                default:
+                    return desc.ImplementationInstance ?? desc.ImplementationFactory(this);
+            }
+        }
+
+        public IEnumerable<T> GetServices<T>() => GetServices(typeof(T)).Select(o => (T)o);
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            foreach (var desc in _descriptors.Where(s => s.ServiceType == serviceType))
+            {
+                switch (desc.Lifetime)
+                {
+                    case ServiceLifetime.Singleton:
+                        yield return _services.GetOrAdd(desc.ServiceType, desc.ImplementationFactory(this));
+                        break;
+                    default:
+                        yield return desc.ImplementationInstance ?? desc.ImplementationFactory(this);
+                        break;
+                }
+            }
+        }
+
+        public IEnumerable<T> GetAllServices<T>() => _services.Values.Where(o => o is T).Select(o => (T)o);
+        public IEnumerable<object> GetAllServices() => _services.Values;
+    }
+}
